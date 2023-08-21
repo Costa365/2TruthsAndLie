347041bc97a919d161c2268f47fe2f4b6acc4205
Costa Constantinou <costa365@gmail.com>
2023-08-21 22:23:10 +0300

    Added websocket test

--- backend/app/test_game.py
+++ backend/app/test_game.py
@@ -1,5 +1,5 @@
-
 import requests
+import websocket
 
 ENDPOINT = 'http://127.0.0.1:8000'
 
@@ -7,7 +7,7 @@ def test_endpoint():
     response = requests.get(ENDPOINT)
     data = response.json()
     assert response.status_code == 200
-    assert 'Team Games API v0.1' in data['message']
+    assert 'Team Games API' in data['message']
     
 
 def test_create_game():
@@ -17,3 +17,17 @@ def test_create_game():
     data = response.json()
     assert response.status_code == 200
     assert 'id' in data and data.get('id') is not None
+
+
+def test_join_games():
+    url = ENDPOINT + '/game'
+    body = {'type':'2 Truths And A Lie', 'name':'costa'}
+    response = requests.post(url, json = body)
+    data = response.json()
+    assert response.status_code == 200
+    assert 'id' in data and data.get('id') is not None
+    gid = data['id']
+
+    ws = websocket.WebSocket()
+    ws.connect(f'ws://localhost:8000/ws/{gid}/Kugan')
+    ws.close()
