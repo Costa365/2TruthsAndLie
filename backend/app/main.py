89f62b700dfd9a72a8b8b404753f029d92eefbf8
Costa Constantinou <costa365@gmail.com>
2023-09-02 20:05:39 +0300

    Create game from frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
 import app.schemas as schemas
 from app.games import Games
 
 app = FastAPI()
 games = Games()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
