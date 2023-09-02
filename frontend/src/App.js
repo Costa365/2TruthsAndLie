0dc3f283bb621dafc62254c2ae42bd8c201268cf
Costa Constantinou <costa365@gmail.com>
2023-09-02 15:39:17 +0300

    Added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Test 1234</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
