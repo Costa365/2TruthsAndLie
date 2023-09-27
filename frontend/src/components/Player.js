b80e2ebd1dfe4bc484be98736f7cbacde10b11af
Costa Constantinou <costa365@gmail.com>
2023-09-27 17:57:46 +0300

    Updated styling

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -7,7 +7,7 @@ import guessedIcon from '../images/guessed.png'
 
 function Player({name, online, played, guessed, isMe, isFaciliator}) {
   return (
-    <div className={isMe=='true'?'player-div-you':'player-div'}>
+    <div className={isMe==='true'?'player-div-you':'player-div'}>
       {name}
       <img className={online==='Online'?'img-player-on':'img-player-off'} src={connectedIcon} alt="online" title="Connected?" />
       {isFaciliator==='true'?<img className='img-player-on' src={facilitatorIcon} alt="facilitator" title="Facilitator" /> : <span /> }
