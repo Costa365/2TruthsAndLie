b1abc6a09dc50c0fb6abe6ffc923aee16db1b486
Costa Constantinou <costa365@gmail.com>
2023-10-04 21:51:56 +0300

    Add link to header image

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
   
   return (
     <header className='App-header'>
-      <img src={logo} className='App-logo' alt='logo' />
+      <a href="/"><img src={logo} className='App-logo' alt='logo' /></a>
     </header>
   );
 }
