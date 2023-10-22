e23af28fa34c1c0635253c8b56ea73ee78613377
Costa Constantinou <costa365@gmail.com>
2023-10-22 06:47:15 +0300

    Added ws connectiom retries

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -187,7 +187,16 @@ function Game() {
 
     onError: (error) => {
       setWsStatus('ERROR');
-    }
+    },
+
+    shouldReconnect: (closeEvent) => true,
+    reconnectAttempts: 10,
+    // attemptNumber will be 0 the first time it attempts to reconnect, so this equation 
+    // results in a reconnect pattern of 1 second, 2 seconds, 4 seconds, 8 seconds, and 
+    // then caps at 10 seconds until the maximum number of attempts is reached
+    reconnectInterval: (attemptNumber) =>
+      Math.min(Math.pow(2, attemptNumber) * 1000, 10000),
+
   });  
 
   const handleTtlSubmit = (data) => {
